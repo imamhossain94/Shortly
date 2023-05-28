c6ba5e43da8771bc0e085ab302e47d0cc1188c99
imamhossain94 <imamagun94@gmail.com>
2023-05-28 17:37:59 +0600

    Project name updated to Shortly

---
+++ app/src/main/java/com/newagedevs/url_shortener/view/base/FragmentInfo.kt
@@ -0,0 +1,8 @@
+package com.newagedevs.url_shortener.view.base
+
+interface FragmentInfo {
+    val layoutResId: Int
+    val iconResId: Int
+    val title: CharSequence?
+    val isAppBarEnabled: Boolean
+}
