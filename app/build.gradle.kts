fbf9c39e97dc2d6ace846ee859623b287e44371c
Md Imam Hossain <imamagun94@gmail.com>
2024-08-21 22:53:18 +0600

    ui fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.newagedevs.url_shortener"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
