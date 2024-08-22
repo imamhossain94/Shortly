3b43d19893fc7e8959ed5a59106fbf3fe49bfdec
Md Imam Hossain <imamagun94@gmail.com>
2024-08-22 23:38:05 +0600

    app version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.newagedevs.url_shortener"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.0.2"
+        versionCode = 11
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
